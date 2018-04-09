7ed56a5921e7b8973a60bbc7cf7e8f1bf30c2409
lynnf1 <linnf@live.com>
2018-04-09 17:28:25 -0500

    Changed nav, added differnet styling across sevral pages, added signup for mailing list

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Hawleyville Kennels
 This is a application created for my grandparents so that they would easily be able to list when they had new puppies for sale.
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,45 +4,41 @@ User.create!(name: "Linsey F",email: 'test1@test.com',password: '123456',passwor
 p "1 user made"
 
 1.times do |pup|
-  Pup.create(breed: "German Shepard", body: "1 month old", price: 100, tag: "Unsold", image_file_name: "gmpup2.jpg", image_content_type: "image/jpeg")
+  Pup.create(breed: "German Shepard", body: "1 month old test", price: 100, tag: "Unsold")
 end
 4.times do |pup|
-  Pup.create(breed: "Dachshund", body: "1 month old", price: 300, tag: "Unsold", image_file_name: "pup1.jpg", image_content_type: "image/jpeg")
-end
-
-2.times do |pup|
-  Pup.create!(breed: "Dachshund", body: "1 month old", price: 300, tag: "Sold", image_file_name: "pup1.jpg", image_content_type: "image/jpeg")
+  Pup.create(breed: "Dachshund", body: "1 month old", price: 300, tag: "Unsold")
 end
 
 p "All puppies have formed"
 
 
 1.times do |post|
-  Post.create!(title: "Dasie", body: "2 years old, Male", image_file_name: "dasie.jpg")
+  Post.create!(title: "Dasie", body: "2 years old, Male")
 end
 
 1.times do |post|
-  Post.create!(title: "Cowboy", body: "2 years old, Male", image_file_name: "cow.jpg")
+  Post.create!(title: "Cowboy", body: "2 years old, Male")
 end
 
 1.times do |post|
-  Post.create!(title: "Max", body: "2 years old, Male", image_file_name: "gms.jpg")
+  Post.create!(title: "Max", body: "2 years old, Male")
 end
 
 1.times do |post|
-  Post.create!(title: "Dolly", body: "1 years old, Female", image_file_name: "dolly.jpg")
+  Post.create!(title: "Dolly", body: "1 years old, Female")
 end
 
 1.times do |post|
-  Post.create!(title: "Bo", body: "2 years old, Male", image_file_name: "bo.jpg")
+  Post.create!(title: "Bo", body: "2 years old, Male")
 end
 
 1.times do |post|
-  Post.create!(title: "Dash", body: "2 years old, Male", image_file_name: "dash.jpg")
+  Post.create!(title: "Dash", body: "2 years old, Male")
 end
 
 1.times do |post|
-  Post.create!(title: "Filly", body: "2 years old, Male", image_file_name: "filly.jpg")
+  Post.create!(title: "Filly", body: "2 years old, Male")
 end
 
 p "All dogs have formed"
